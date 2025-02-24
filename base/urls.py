ffa003c0663385d89d45097a3156012f356b2c61
Arkhival <Dylanlaborante@gmail.com>
2025-02-24 03:53:32 -0500

    DJANGO Rest Framework

--- base/urls.py
+++ base/urls.py
@@ -16,4 +16,7 @@ urlpatterns = [
     path('delete-message/<str:pk>/', views.deleteMessage, name="delete-message"),
 
     path('update-user/', views.updateUser, name="update-user"),
+
+    path('topic/', views.topicsPage, name="topics"),
+    path('activity/', views.activityPage, name="activity"),
 ]
